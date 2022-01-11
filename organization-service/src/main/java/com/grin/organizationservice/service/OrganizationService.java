fd587767b66609ef2250d7c3c501dc7d7cbc23aa
axelr0d <stanislav.kotyhrobov@privatbank.ua>
2022-01-11 18:15:29 +0200

    added organization service

---
+++ organization-service/src/main/java/com/grin/organizationservice/service/OrganizationService.java
@@ -0,0 +1,38 @@
+package com.grin.organizationservice.service;
+
+import com.grin.organizationservice.model.Organization;
+import com.grin.organizationservice.repository.OrganizationRepository;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class OrganizationService {
+
+    @Setter(onMethod = @__({@Autowired}))
+    private OrganizationRepository organizationRepository;
+
+    public Organization findById(String organizationId) {
+        Optional<Organization> opt = organizationRepository.findById(organizationId);
+        return opt.orElse(null);
+    }
+
+    public Organization create(Organization organization) {
+        organization.setId(UUID.randomUUID().toString());
+        organization = organizationRepository.save(organization);
+        return organization;
+
+    }
+
+    public void update(Organization organization) {
+        organizationRepository.save(organization);
+    }
+
+    public void delete(Organization organization) {
+        organizationRepository.deleteById(organization.getId());
+    }
+
+}
