8bd4c29a481e4518af4c4923325c18d04ee560bf
axelr0d <stanislav.kotyhrobov@privatbank.ua>
2022-01-15 12:55:10 +0200

    updated organization service - added eureka

--- organization-service/src/main/java/com/grin/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/grin/organizationservice/OrganizationServiceApplication.java
@@ -3,10 +3,13 @@ package com.grin.organizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @RefreshScope
+@EnableDiscoveryClient
 public class OrganizationServiceApplication {
 
     public static void main(String[] args) {
