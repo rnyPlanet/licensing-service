f91da09c258dbc18fbf42ddf77560b5f8f0ffb53
axelr0d <stanislav.kotyhrobov@privatbank.ua>
2022-01-15 13:38:44 +0200

    updated license service. updated pom, added eureka request, feign client, rest template

--- licensing-service/src/main/java/com/grin/licensingservice/service/LicenseService.java
+++ licensing-service/src/main/java/com/grin/licensingservice/service/LicenseService.java
@@ -2,7 +2,11 @@ package com.grin.licensingservice.service;
 
 import com.grin.licensingservice.config.ServiceConfig;
 import com.grin.licensingservice.model.License;
+import com.grin.licensingservice.model.Organization;
 import com.grin.licensingservice.repository.LicenseRepository;
+import com.grin.licensingservice.service.client.OrganizationDiscoveryClient;
+import com.grin.licensingservice.service.client.OrganizationFeignClient;
+import com.grin.licensingservice.service.client.OrganizationRestTemplateClient;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -21,11 +25,18 @@ public class LicenseService {
     @Setter(onMethod = @__({@Autowired}))
     private ServiceConfig config;
 
+    @Setter(onMethod = @__({@Autowired}))
+    OrganizationFeignClient organizationFeignClient;
+    @Setter(onMethod = @__({@Autowired}))
+    OrganizationRestTemplateClient organizationRestClient;
+    @Setter(onMethod = @__({@Autowired}))
+    OrganizationDiscoveryClient organizationDiscoveryClient;
 
     public License getLicense(String licenseId,
-                              String organizationId) {
+                              String organizationId,
+                              String clientType) {
         License license = licenseRepository
-                .findLicenseByOrganizationIdAndLicenseId(organizationId, licenseId);
+                .findLicenseByLicenseId(licenseId);
 
         if (license == null) {
             throw new IllegalArgumentException(
@@ -35,10 +46,49 @@ public class LicenseService {
             );
         }
 
+        Organization organization = retrieveOrganizationInfo(organizationId, clientType);
+        if (null != organization) {
+            license.setOrganization(organization);
+        }
+
         return license.withComment(config.getProperty());
     }
 
-    public License createLicense(License license) {
+    private Organization retrieveOrganizationInfo(String organizationId, String clientType) {
+        Organization organization;
+
+        switch (clientType) {
+            case "feign":
+                System.out.println("I am using the feign client");
+                organization = organizationFeignClient.getOrganization(organizationId);
+                break;
+            case "rest":
+                System.out.println("I am using the rest client");
+                organization = organizationRestClient.getOrganization(organizationId);
+                break;
+            case "discovery":
+                System.out.println("I am using the discovery client");
+                organization = organizationDiscoveryClient.getOrganization(organizationId);
+                break;
+            default:
+                organization = organizationRestClient.getOrganization(organizationId);
+        }
+
+        return organization;
+    }
+
+    public License createLicense(String organizationId, License license) {
+        Organization organization = retrieveOrganizationInfo(organizationId, "");
+
+        if (null == organization) {
+            throw new IllegalArgumentException(
+                    String.format(messageSource.getMessage(
+                            "license.message.create.organizationId.error", null, null),
+                            organizationId)
+            );
+        }
+
+        license.setOrganizationId(organization.getId());
         license.setLicenseId(UUID.randomUUID().toString());
         licenseRepository.save(license);
 
