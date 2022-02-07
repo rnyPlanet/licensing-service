478605c313eca6f4d1de62d5fccc678dabff2b68
rnyPlanet <workautu@gmail.com>
2022-02-07 19:06:57 +0200

    fix mvn

--- organization-service/src/test/java/OrganizationServiceApplicationTests.java
+++ organization-service/src/test/java/OrganizationServiceApplicationTests.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
 class OrganizationServiceApplicationTests {
 
 	@Test
